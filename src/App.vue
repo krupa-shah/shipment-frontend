7e7c9c5df9571c271723f44c8ec95d8759375850
Krupa Shah <krupa@twinhealth.com>
2022-05-23 10:50:47 -0700

    Renamed HelloWorld.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ShipmentUtility/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ShipmentUtility from './components/ShipmentUtility.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ShipmentUtility
   }
 }
 </script>
